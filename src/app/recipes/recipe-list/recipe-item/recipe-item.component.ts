21b9de48c796c0b8188b843b6ad2cff2cdf9eab9
aniket-abmiro <aniket.pawar@abmiro.in>
2023-11-30 10:47:20 +0530

    section4

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../../recipe.model';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'this is simple test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*')
+  ];
+  constructor() {
+
+  }
+}
