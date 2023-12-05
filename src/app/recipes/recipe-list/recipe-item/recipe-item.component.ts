05b2683a319babef727af3aa31df341269ecd047
aniket-abmiro <aniket.pawar@abmiro.in>
2023-12-05 18:01:11 +0530

    section 12

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,13 +10,11 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
-  constructor(private recipeService: RecipeService) {
+  @Input() index: number;
 
-  }
+
   ngOnInit(): void {
 
   }
-  onSelect() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+
 }
